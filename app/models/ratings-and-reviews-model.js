1eaaeab6056aa63796f39ac46087e4e639c41cd4
Cooper David <dcooper@technisys.com>
2020-09-25 14:30:52 -0300

    fix schema name

--- app/models/ratings-and-reviews-model.js
+++ app/models/ratings-and-reviews-model.js
@@ -10,4 +10,4 @@ const ratingsAndReviewsSchema = new Schema({
   rating: { type: Number, required: true }
 });
 
-module.exports = mongoose.model('reviews', ratingsAndReviewsSchema);
+module.exports = mongoose.model('reviews', ratingsAndReviewsSchema, 'Reviews');
