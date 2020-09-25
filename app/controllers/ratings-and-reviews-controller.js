5e5a79970d8ec763645dce8bd468e67d36371354
Nicolas Agustin Perez <nicperez@falabella.com.ar>
2020-09-25 14:59:35 -0300

    feat: added review-by-id

--- app/controllers/ratings-and-reviews-controller.js
+++ app/controllers/ratings-and-reviews-controller.js
@@ -1,10 +1,19 @@
-const RatingsAndReviews = require('../models/ratings-and-reviews-model');
+const RatingsAndReviews = require('../models/ratings-and-reviews-model'); 
 
 module.exports.createRatingsAndReviews = async (review) => {
   try {
     const newReview = new RatingsAndReviews(review);
     const result = await newReview.save();
     return result;
+  } catch (error) {
+    throw error;
+  }
+};
+
+module.exports.findRatingsAndReviews = async (id) => {
+  try {
+    const review = RatingsAndReviews.findById(id);
+    return review;
   } catch (error) {
     throw error;
   }
--- app/routes/api-route.js
+++ app/routes/api-route.js
@@ -1,10 +1,12 @@
 const Router = require('express').Router;
 const { reviewValidation } = require('../middlewares/validations/review-validation-middleware');
 const { ratingsAndReviewsMiddleware } = require('../middlewares/ratings-and-reviews-middleware');
+const { findReviewByIdMiddleware } = require('../middlewares/find-review-by-id-middleware');
 const { responseMiddleware } = require('../middlewares/response/response-middleware');
 
 const router = Router();
 
 router.post('/ratings-and-reviews', [reviewValidation, ratingsAndReviewsMiddleware, responseMiddleware]);
+router.get('/ratingsAndReviews/:reviewId', [findReviewByIdMiddleware, responseMiddleware]);
 
 module.exports = router;
