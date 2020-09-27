39c0c2c4373a486863c1b7dccc19d1ec3d6c0d28
Nicolas Agustin Perez <nicperez@falabella.com.ar>
2020-09-27 02:11:44 -0300

    feat: added

--- app/constants/enums.js
+++ app/constants/enums.js
@@ -1,9 +1,17 @@
 module.exports.ENUMS = {
-    MOST_RECENT: "most recent",
-    HIGHEST_RATED: "highest rated",
-    LOWEST_RATED: "lowest rated",
-    CREATED_AT: "createdAt",
-    RATING: "rating",
-    ASC: "asc",
-    GOOD: 'good',
+  MOST_RECENT: 'most recent',
+  HIGHEST_RATED: 'highest rated',
+  LOWEST_RATED: 'lowest rated',
+  CREATED_AT: 'createdAt',
+  RATING: 'rating',
+  ASC: 'asc',
+  GOOD: 'good',
+};
+
+module.exports.REPORT_TYPES = {
+  RACISM: 'RACISM',
+  HATE_SPEECH: 'HATE_SPEECH',
+  BAD_WORDS: 'BAD_WORDS',
+  SPAM: 'SPAM',
+  OTHER: 'OTHER',
 };
